76f8fa350b753805a1e7b96ee62ba2264c3710f8
ImrulEmon <emimrulemon@gmail.com>
2022-03-10 01:39:45 +0600

    3️⃣✔

--- Module-31.5/HomeWork.js
+++ Module-31.5/HomeWork.js
@@ -21,3 +21,23 @@ const intro = `${name} is ${kid.age} years old.
 He is a ${kid.job}.
 He is in class ${kid.cls}.`;
 print(intro);
+
+// ------3-------------
+// ----3.1----
+const oneP = n => console.log(n*5);
+oneP(5);
+
+// ----3.2----
+const twoP = (x,y) => (x+2)*(y+2);
+print(twoP(2,2));
+// ----3.3----
+const threeP = (a,b,c)=> a*b*c;
+print(threeP(2,3,10));
+// ----3.4----
+const twoPT = (x,y) => {
+    const rx = x+2;
+    const ry = y+2;
+    const r =rx * ry;
+    return r;
+};
+print(twoPT(3,3));
