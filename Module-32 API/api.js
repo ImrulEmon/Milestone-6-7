e9b1c188e49853775006c1df7ff4b74dc6547476
ImrulEmon <emimrulemon@gmail.com>
2022-03-13 02:08:12 +0600

    API-showing data on website

--- Module-32 API/api.js
+++ Module-32 API/api.js
@@ -3,3 +3,29 @@ function loadData() {
         .then(respose => respose.json())
         .then(json => console.log(json));
 }
+function loadUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(users => displayUsers(users)
+            /*users.forEach(user => 
+            console.log(`${user.name} | ${user.username}`))*/);
+}
+function loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(posts => displayPosts(posts));
+}
+
+function displayUsers(users) {
+    const ul = document.getElementById("users");
+   for(const user of users){
+       console.log(user.name);
+       const li = document.createElement('li');
+       li.classList.add("li");
+       li.innerText= `Name : ${user.name} | Email : ${user.email}`;
+       ul.appendChild(li);
+   }
+}
+function displayPosts(posts) {
+    posts.forEach(post=> console.log(post.title))
+}
