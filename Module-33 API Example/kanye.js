5f25effca587de4fbb3ae827d8f6876d6571917f
ImrulEmon <emimrulemon@gmail.com>
2022-03-14 00:02:56 +0600

    kanye

---
+++ Module-33 API Example/kanye.js
@@ -0,0 +1,11 @@
+function kanye(){
+    fetch('https://api.kanye.rest')
+    .then(res=>res.json())
+    .then(quote=>displayQuote(quote));
+}
+
+function displayQuote(quote){
+   const quotex = document.getElementById("quote");
+   quotex.innerText=quote.quote;
+
+}
