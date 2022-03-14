fde12f80d1719ecb6b1eb2b536ccaa66fc9b21ea
ImrulEmon <emimrulemon@gmail.com>
2022-03-15 02:56:44 +0600

    CLICK ON DIV TO SHOW INFO⭐⭐⭐

--- Module-33.5/practise.js
+++ Module-33.5/practise.js
@@ -4,14 +4,42 @@ const loadComment =()=>{
     .then(data=>displayComment(data))
 }
 
+const displayInfo= info =>{
+    const infoBox=document.getElementById('CommentInfo').innerText=info;
+    // const p = document.createElement('p');
+    // p.innerText=info;
+    // infoBox.appendChild(p);
+}
+
+const loadInfo=id=>{
+    console.log(id);
+    const url = `https://jsonplaceholder.typicode.com/comments?id=${id}`;
+   fetch(url)
+   .then(res=>res.json())
+   .then(data=>displayInfo(data[0].body));
+}
+
+
 
 const displayComment = comments =>{
     const commentBox = document.getElementById('commentBox');
     comments.forEach(comment => {
-        const p = document.createElement('p');
-        p.innerHTML=`${comment.id} | ${comment.name} | ${comment.email}
+        const div = document.createElement('div');
+        div.onclick=function(){loadInfo(comment.id)};
+        div.innerHTML=`
+        ${comment.id} | ${comment.name} | ${comment.email} |  ${comment.postId}
+        <button onclick="loadInfo(${comment.id})">Comment Info</button>
         `;
-        commentBox.appendChild(p);
+        div.classList.add("singleComment");
+        commentBox.appendChild(div);
         //console.log(comment.name,comment.email);
     });
 }
+
+
+// document.getElementById("commentBox").addEventListener('click', function (event) {
+//     if (event.target.className == 'singleComment') {
+//         console.log(event.target.innerHTML);
+//     }
+
+// })
