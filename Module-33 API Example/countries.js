af123397261479e6ab66b6c2710b915e983c3749
ImrulEmon <emimrulemon@gmail.com>
2022-03-14 16:31:17 +0600

    COUNTRY-WATCH THIS⭐⭐⭐

--- Module-33 API Example/countries.js
+++ Module-33 API Example/countries.js
@@ -7,29 +7,41 @@ const loadCountries = () => {
 const displayCountries = countries => {
     const countryContainer = document.getElementById('countryContainer');
     for (const country of countries) {
-        const language = JSON.stringify(country.languages).replace(/[^a-zA-Z0-9:, ]/g, '');
-
-
+        // const language = JSON.stringify(country.languages).replace(/[^a-zA-Z0-9:, ]/g, '');
         const p = document.createElement('p');
         p.innerHTML = `${country.name.common} | 
         ${country.capital} | ${country.region} |
-        ${language}
+         | 
+        <button onclick="loadCountryDetail('${country.name.common}')">Details</button>
         `;
         p.classList.add("country");
         countryContainer.appendChild(p);
         //console.log(country.region)
-    }
+    };
 }
+
+const loadCountryDetail = name => {
+    const url = `https://restcountries.com/v3.1/name/${name}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayInfo(data[0]));
+}
+
+
+const displayInfo = country => {
+    const info = document.getElementById("info");
+    info.innerHTML = `<img width='200px' src="${country.flags.png}">
+    <p>Population : ${country.population} | ${country.subregion} | ${country.startOfWeek}</p>
+    `;
+}
+
 
 document.getElementById("countryContainer").addEventListener('click', function (event) {
     if (event.target.tagName == 'P') {
         event.target.style.fontWeight = "bold";
         event.target.style.backgroundColor = "Green";
 
-        // font-weight: bold;
-        //     background-color: black;
-        //     color: white;
-        //     transform: scale(1.1);
+
     }
 
 })
@@ -38,10 +50,6 @@ document.getElementById("countryContainer").addEventListener('dblclick', functio
         event.target.style.fontWeight = "normal";
         event.target.style.backgroundColor = "white";
 
-        // font-weight: bold;
-        //     background-color: black;
-        //     color: white;
-        //     transform: scale(1.1);
     }
 
 })
