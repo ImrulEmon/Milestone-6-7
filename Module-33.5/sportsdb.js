e0d5458178e18828b4072bceafaf908341d15b31
ImrulEmon <emimrulemon@gmail.com>
2022-03-15 22:58:46 +0600

    ⚽

--- Module-33.5/sportsdb.js
+++ Module-33.5/sportsdb.js
@@ -1,19 +1,22 @@
-const loadClubs=()=>{
+const loadClubs = () => {
     const country = document.getElementById("countryName").value;
-    const url =`https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?s=Soccer&c=${country}`;
+    const url = `https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?s=Soccer&c=${country}`;
     fetch(url)
-    .then(res=>res.json())
-    .then(clubs=>displayClubs(clubs.teams))
+        .then(res => res.json())
+        .then(clubs => displayClubs(clubs.teams))
 }
 
-const displayClubs=clubs=>{
+
+
+
+const displayClubs = clubs => {
     const searchResult = document.getElementById("searchResult");
-    searchResult.textContent='';
-    clubs.forEach(club => {
+    searchResult.textContent = '';
+    clubs.forEach(club => {;
         const div = document.createElement('div');
         div.classList.add('col');
-        div.innerHTML=`
-        <div class="card h-100">
+        div.innerHTML = `
+        <div class="card h-100"">
         <img src="${club.strTeamBadge}" class="card-img-top img-fluid my-4 w-50 mx-auto" alt="...">
         <div class="card-body">
           <h5 class="card-title">${club.strTeam}</h5>
@@ -21,7 +24,9 @@ const displayClubs=clubs=>{
           <p class="card-text">League : ${club.strLeague}</p>
           <p class="card-text">Stadium : ${club.strStadium}</p>
           <p class="card-text">Location : ${club.strStadiumLocation}</p>
-        </div>
+          <p class="card-text">Stadium Capacity : ${club.intStadiumCapacity}</p>
+          </div>
+          <button class="btn btn-primary" onclick="ClubInfo('${club.strAlternate}')">More Info</button>
       </div>
         `;
         searchResult.appendChild(div);
@@ -31,9 +36,13 @@ const displayClubs=clubs=>{
 
 //Enter Event
 var input = document.getElementById("countryName");
-    input.addEventListener("keypress", function(event) {
-      if (event.keyCode === 13) {
+input.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13) {
         event.preventDefault();
         document.getElementById("button-search").click();
-      }
-    });
+    }
+});
+
+const ClubInfo=info=>{
+    alert(info);
+}
