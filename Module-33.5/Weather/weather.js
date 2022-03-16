679cb7fc2fa7f34b08937460a396141853bb3add
ImrulEmon <emimrulemon@gmail.com>
2022-03-16 17:10:36 +0600

    ☁⛅

--- Module-33.5/Weather/weather.js
+++ Module-33.5/Weather/weather.js
@@ -5,17 +5,18 @@ const loadWeather = () => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=906085e45b2637bd8bb9c994319d73fd`)
         .then(res => res.json())
         .then(data => displayWeather(data));
+        document.getElementById('city').value='';
 }
 
 
  const displayWeather = data =>{
     const icon =data.weather[0].icon;
      document.getElementById('weather').innerHTML=`
-    <img src='http://openweathermap.org/img/wn/${icon}@2x.png'>
-    <h3>${data.weather[0].description}<h3>
-     <h2 class="text-bold">${data.main.temp}<sup>o</sup>C</h2>
-    <p>Feels Like : ${data.main.feels_like}<sup>o</sup>C | Max : ${data.main.temp_max}<sup>o</sup>C | Min : ${data.main.temp_min}<sup>o</sup>C</p>
-    <p><small>${data.name},${data.sys.country}</small></p>
+    <img class="infoT" src='http://openweathermap.org/img/wn/${icon}@2x.png'>
+    <h3 class="zoom">${data.weather[0].description.toUpperCase()}<h3>
+     <h2 class="text-bold zoom">${data.main.temp}<sup>o</sup>C</h2>
+    <p class="infoT">Feels Like : ${data.main.feels_like}<sup>o</sup>C | Max : ${data.main.temp_max}<sup>o</sup>C | Min : ${data.main.temp_min}<sup>o</sup>C</p>
+    <p class="zoom"><small>${data.name},${data.sys.country}</small></p>
      `;
     console.log(data.main.feels_like,data.main.temp,data.main.temp_max,data.main.temp_min);
      console.log(data.weather[0].main,data.weather[0].description);
