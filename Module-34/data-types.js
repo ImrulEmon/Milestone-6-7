0a81a4aabfe67c49e5aef68f2dc4576ad912ab2e
ImrulEmon <emimrulemon@gmail.com>
2022-03-17 00:28:59 +0600

    Module 34

--- Module-34/data-types.js
+++ Module-34/data-types.js
@@ -1,19 +1,32 @@
 /**
  * 1.Data types
+ * ##--Primitive(1 value)
  * -Number
  * -string
  * -boolean
  * -undefine
  * -null
- * -object
  * -symbol
+ * ##--Non Primitive
+ * -object
+ * -function
+ * -array
  * 
+ * 2
+ * Truthy:
+ * -positive,negative number
+ * -any string or char or even a gap
+ * -if '0' zero is string
+ * -true
+ * -"true","false"(string)
  * 
- * 
- * 
- * 
- * 
- * 
+ * Falsy:
+ * - 0(zero)
+ * -empty string''
+ * -false
+ * -if variable is undefined
+ * -null
+ * -NaN(Not a Number)
  * 
  * 
  * 
