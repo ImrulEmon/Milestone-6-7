3de102f5aa3ec28acb45933b35fbca351f41e0a9
ImrulEmon <emimrulemon@gmail.com>
2022-03-16 16:26:12 +0600

    ☁⛅

---
+++ Module-33.5/data.js
@@ -0,0 +1,46 @@
+{
+    "coord": {
+        "lon": 91.6667,
+        "lat": 24.5
+    },
+    "weather": [
+        {
+            "id": 800,
+            "main": "Clear",
+            "description": "clear sky",
+            "icon": "01d"
+        }
+    ],
+    "base": "stations",
+    "main": {
+        "temp": 36.28,
+        "feels_like": 33.68,
+        "temp_min": 36.28,
+        "temp_max": 36.28,
+        "pressure": 1003,
+        "humidity": 13,
+        "sea_level": 1003,
+        "grnd_level": 1002
+    },
+    "visibility": 10000,
+    "wind": {
+        "speed": 3.95,
+        "deg": 283,
+        "gust": 3.3
+    },
+    "clouds": {
+        "all": 0
+    },
+    "dt": 1647422693,
+    "sys": {
+        "country": "BD",
+        "sunrise": 1647388913,
+        "sunset": 1647432144
+    },
+    "timezone": 21600,
+    "id": 1477362,
+    "name": "Sylhet Division",
+    "cod": 200
+}
+
+http://openweathermap.org/img/wn/01d@2x.png
--- Module-33.5/weather.js
+++ Module-33.5/weather.js
@@ -2,13 +2,22 @@
 // const key=906085e45b2637bd8bb9c994319d73fd;
 const loadWeather = () => {
     const city = document.getElementById('city').value;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=906085e45b2637bd8bb9c994319d73fd`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=906085e45b2637bd8bb9c994319d73fd`)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displayWeather(data));
 }
 
 
  const displayWeather = data =>{
-    console.log(data.main.feels_like)
+    const icon =data.weather[0].icon;
+     document.getElementById('weather').innerHTML=`
+    <img src='http://openweathermap.org/img/wn/${icon}@2x.png'>
+    <h3>${data.weather[0].description}<h3>
+     <h2 class="text-bold">${data.main.temp}<sup>o</sup>C</h2>
+    <p>Feels Like : ${data.main.feels_like}<sup>o</sup>C | Max : ${data.main.temp_max}<sup>o</sup>C | Min : ${data.main.temp_min}<sup>o</sup>C</p>
+    <p><small>${data.name},${data.sys.country}</small></p>
+     `;
+    console.log(data.main.feels_like,data.main.temp,data.main.temp_max,data.main.temp_min);
      console.log(data.weather[0].main,data.weather[0].description);
+     
  }
