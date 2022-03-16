42675fdef7d43d8c316789ec6ed8460150d3cd65
ImrulEmon <emimrulemon@gmail.com>
2022-03-17 01:31:53 +0600

    closure

---
+++ Module-34/Null vs Undefined.js
@@ -0,0 +1,13 @@
+// Undefined ⬇
+/**
+ * 1)Value not assigned
+ * 2)function didn't return
+ * 3)parameter is not passed
+ */
+let first;
+console.log(first);
+
+function add(a,b){
+    const c =a+b;
+}
+console.log(add());
---
+++ Module-34/closure.js
@@ -0,0 +1,10 @@
+function stopWatch(){
+    let counter = 0;
+    return function () {
+        counter ++;
+        return counter;
+    }
+}
+const clock1=stopWatch();
+console.log(clock1());
+console.log(clock1());
