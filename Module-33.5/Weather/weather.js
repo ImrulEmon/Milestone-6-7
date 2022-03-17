dd1c793d13c7efd61b7c15283b5c5428e61f77ad
ImrulEmon <emimrulemon@gmail.com>
2022-03-17 15:41:02 +0600

    :cloud: :sun:

--- Module-33.5/Weather/weather.js
+++ Module-33.5/Weather/weather.js
@@ -1,6 +1,7 @@
-
+document.getElementById('load').style.display='none';
 // const key=906085e45b2637bd8bb9c994319d73fd;
 const loadWeather = () => {
+    document.getElementById('load').style.display='block';
     const city = document.getElementById('city').value;
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=906085e45b2637bd8bb9c994319d73fd`)
         .then(res => res.json())
@@ -11,16 +12,17 @@ const loadWeather = () => {
 
  const displayWeather = data =>{
     const icon =data.weather[0].icon;
-     document.getElementById('weather').innerHTML=`
+     const info=document.getElementById('weather');
+     info.textContent='';
+     info.innerHTML=`
     <img class="infoT" src='http://openweathermap.org/img/wn/${icon}@2x.png'>
     <h3 class="zoom">${data.weather[0].description.toUpperCase()}<h3>
-     <h2 class="text-bold zoom">${data.main.temp}<sup>o</sup>C</h2>
+     <h2 class="text-bold zoom">${(data.main.temp)}<sup>o</sup>C</h2>
     <p class="infoT">Feels Like : ${data.main.feels_like}<sup>o</sup>C | Max : ${data.main.temp_max}<sup>o</sup>C | Min : ${data.main.temp_min}<sup>o</sup>C</p>
     <p class="zoom"><small>${data.name},${data.sys.country}</small></p>
      `;
-    console.log(data.main.feels_like,data.main.temp,data.main.temp_max,data.main.temp_min);
-     console.log(data.weather[0].main,data.weather[0].description);
-     
+    
+     document.getElementById('load').style.display='none';
  }
 
 let input = document.getElementById("city");
@@ -30,3 +32,4 @@ input.addEventListener("keypress", function (event) {
         document.getElementById("button-search").click();
     }
 });
+
