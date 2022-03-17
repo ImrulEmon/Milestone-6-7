7cf562ec70ededcd3a2d60771f7ae298f819bc4f
ImrulEmon <emimrulemon@gmail.com>
2022-03-17 18:39:22 +0600

    Method

---
+++ Module-35 Obj Concept/Method.js
@@ -0,0 +1,21 @@
+const student ={
+    id:101,
+    name:"Ponting",
+    money:1500,
+    major : "Math",
+    isRich: false,
+    subject :["english","calculas",'botany'],
+    bestFriend : {
+        name:"Fakir Mia",
+        major : 'English'
+    },
+    takeExam: function () {
+        console.log(this.bestFriend.name,"Taking exam");
+    },
+    treat : function (expense) {
+        return this.money-expense;
+    }
+}
+console.log(student.takeExam())
+const remain = student.treat(500);
+console.log(remain);
