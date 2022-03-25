f724c4da60454028b4ca92e3a3a28505567bbfee
ImrulEmon <emimrulemon@gmail.com>
2022-03-25 14:44:11 +0600

    :bell::bell::bell:

--- RegEx/consoleAPI.js
+++ RegEx/consoleAPI.js
@@ -31,7 +31,8 @@ console.dirxml(document);
 -------------
 console.error(object [, object, ...])
 console.error("Error occured");
-----------------------------------*/
+
+-----------------------
 const label = 'Anime Character';
 
 console.group(label);
@@ -41,4 +42,10 @@ console.info('mikasa');
 console.info('levi');
 console.info('conny');
 console.info('hanzo');
+----------------------------------*/
 
+const a={first: 'Rene',last:'Migrtte'};
+const b={first:'Chaim',last:'Soutine',birthday:'18930113'};
+const c={first:'Henri',last:'Ron'};
+
+console.table([a,b,c]);
