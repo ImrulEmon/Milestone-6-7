9f9fc8fa31bab46c7c88c983bf4edfe7e6ef880a
ImrulEmon <emimrulemon@gmail.com>
2022-03-26 19:33:25 +0600

    TS

--- Module-41/first.ts
+++ Module-41/first.ts
@@ -1,2 +1,15 @@
-const myNumber = 5;
-console.log('hello from TS',myNumber);
+// const myNumber = 5;
+// let money = 2000;
+// money = 700;
+// let myName='jake'
+// myName='Eren';
+// console.log('hello from TS',myNumber);
+
+const myNumber:number = 5;
+let money = 2000;
+money = 700;
+let myName:string='jake'
+myName='Eren';
+let boo:boolean=false;
+boo=true;
+console.log('hello from TS',myNumber,myName,money,boo);
--- Module-41/js/first.js
+++ Module-41/js/first.js
@@ -1,4 +1,15 @@
 "use strict";
+// const myNumber = 5;
+// let money = 2000;
+// money = 700;
+// let myName='jake'
+// myName='Eren';
+// console.log('hello from TS',myNumber);
 const myNumber = 5;
-const myName = 'Janibul';
-console.log('hello from TS', myNumber,myName);
+let money = 2000;
+money = 700;
+let myName = 'jake';
+myName = 'Eren';
+let boo = false;
+boo = true;
+console.log('hello from TS', myNumber, myName, money, boo);
