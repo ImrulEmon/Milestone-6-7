28e5a269a3497103b76efa9bec198bcc1c251b84
ImrulEmon <emimrulemon@gmail.com>
2022-03-28 00:15:03 +0600

    4️⃣2️⃣

--- Module-42/destructuring.js
+++ Module-42/destructuring.js
@@ -2,3 +2,18 @@ const numbers =[1,2,3,4,5];
 const [x,y]= numbers;
 const [a,b]= [1,2,3,4,5];
 console.log(x,y,a,b);
+
+const boxify = (n1,n2)=> [n1,n2];
+
+console.log(boxify(3,4));
+
+const student={
+    id:101,
+    name:'jon',
+    age:9,
+    sub:["eng",'ban','math']
+};
+
+const [s1,s2,s3]=student.sub;
+console.log(s1,s2,s3);
+
