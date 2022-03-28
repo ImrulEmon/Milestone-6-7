3f1b9f4a7eff9143d98d94c3a763f751a876496f
ImrulEmon <emimrulemon@gmail.com>
2022-03-28 20:36:07 +0600

    4️⃣2️⃣�5️⃣

---
+++ Module-42.5/prectice.js
@@ -0,0 +1,33 @@
+/**
+ * 1.how JS code is executed
+ * 2.how js handles synchronous and asynchronous calls
+ * 3.setTime out,setInterval
+ * 
+ * --------------
+ * 4.Browser API
+ * --------------
+ * 5.alert,modal,prompt,confirm
+ * 6.name of different parts of url
+ * 7.url parse
+ * 8.location api
+ * 9.how to change the url using location api
+ * 10.use history api to go 2 steps back
+ * 11.sessionStorage vs localStorage
+ * 12.store an object sessionStorage
+ * 13.access the stored object sessionStorage
+ * 
+ * ------------
+ * Debug
+ * ------------
+ * 14.how to find  element that has a specific class name
+ * 15.conditional breakpoint
+ * 16.console log each call to a function(do not break)
+ * 17.explore dev tool official documentation and find out how you could be able to find out the time required to execute for one million time.
+ * 18.typescript vs javascript
+ * when should use typeScript
+ * -----------------------
+ * # Issue tracker debug
+ * # slider debug
+ * -----------------------
+ * 
+ */
