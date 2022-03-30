1cd1bab375b2040c61e25a55645c22af9349b70f
ImrulEmon <emimrulemon@gmail.com>
2022-03-31 01:29:12 +0600

    4️⃣3️⃣ slider

---
+++ Module-43/slider.js
@@ -0,0 +1,23 @@
+const images = [
+    'images/1.jpg',
+    'images/2.jpg',
+    'images/3.jpg',
+    'images/4.jpg',
+    'images/5.jpg',
+    'images/6.jpg',
+    'images/7.jpg',
+    'images/8.jpg',
+    'images/9.jpg'
+];
+const imgElement = document.getElementById('slider-img');
+let imgIndex = 0;
+setInterval(()=>{
+    if(imgIndex>images.length-1){
+        imgIndex=0;
+    }
+    const imgUrl = images[imgIndex];
+    imgElement.setAttribute('src',imgUrl);
+
+    console.log(imgUrl);
+    imgIndex++;
+},1000)
